18accc17e69b0e073013baf28fe942bac9a719b7
Tracy Rotton <tracy@taupecat.com>
2012-09-13 14:23:09 -0400

    Initial commit of TC Custom Taxonomy Filter

---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== TC Custom Taxonomy Filter ===
+Contributors: taupecat
+Requires at least: 3.4
+Tested up to: 3.4.2
+Stable tag: 1
+Tags: custom-taxonomy, filter
+
+Filter your posts by your custom taxonomies.
+
+== Description ==
+
+Filter your posts (including custom post types) by your custom taxonomies, just like you can with WordPress' native category filter.
+
+== Installation ==
+
+1. Place the tc-custom-taxonomy-filter directory inside your plugins directory.
+2. Navigate to the Plugins section of the Dashboard and click "Activate".
+3. There is no step 3!
+
+== Changelog ==
+
+= Version 1.0 =
+* Initial release.
---
+++ tc-custom-taxonomy-filter.php
@@ -0,0 +1,73 @@
+<?php
+/*
+Plugin Name: TC Custom Taxonomy Filter
+Description: Filter your posts (including custom post types) by your custom taxonomies, just like you can with WordPress' native category filter.
+Author: Tracy Rotton
+Version: 1.0
+Author URI: http://www.taupecat.com/
+License: GPLv2
+*/
+
+// Only run this on the admin side of the world
+if ( is_admin ) {
+    add_action( 'restrict_manage_posts', 'tc_ctf_restrict_manage_posts' );
+    add_filter( 'parse_query', 'tc_ctf_convert_taxonomy_id_to_slug' );
+}
+
+// Create our drop-down category listing, using all the latest & greatest WordPress things
+function tc_ctf_restrict_manage_posts() {
+
+    global $wp_query;
+
+    $filters = tc_ctf_get_filters();
+
+    foreach( $filters as $tax_slug ) {
+
+        $taxonomy = get_taxonomy( $tax_slug );
+        $term = get_term_by( 'slug', $wp_query->query_vars[$tax_slug], $taxonomy->name );
+
+        wp_dropdown_categories(
+            array(
+                'show_option_all'   => __("Show All " . $taxonomy->labels->name ),
+                'taxonomy'          => $tax_slug,
+                'name'              => $tax_slug,
+                'show_count'        => true,
+                'hide_empty'        => false,
+                'orderby'           => 'name',
+                'order'             => 'ASC',
+                'show_option_none'  => __("Uncategorized"),
+                'selected'          => $term->term_id
+            )
+        );
+    }
+}
+
+// Get only the custom taxonomies associated with this post type
+function tc_ctf_get_filters() {
+    global $typenow;
+
+    $args = array(
+        'object_type'   => array( $typenow ),
+        '_builtin'      => false
+    );
+
+    $taxonomies = get_taxonomies( $args );
+
+    return $taxonomies;
+}
+
+// Now, actually filter out the posts based on the category, er, _custom taxonomy_ we chose
+
+function tc_ctf_convert_taxonomy_id_to_slug( $query ) {
+    global $pagenow;
+
+    $query_vars = &$query->query_vars;
+    $tax_queries  = &$query->tax_query->queries;
+
+    // loop through each of the requested taxonomies
+    if ( $pagenow == 'edit.php' ) {
+        foreach ( $tax_queries as $tax_query ) {
+            $query_vars[$tax_query['taxonomy']] = get_term_by( 'id', $tax_query['terms'][0], $tax_query['taxonomy'] )->slug;
+        }
+    }
+}
